b880ff1a88cee245bbecf456be8e769a859824c8
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-05 23:56:49 +0000

    Exercise: Error Handling with RecyclerView

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -29,16 +29,18 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.lang.Exception
 
+enum class MarsApiStatus {LOADING, DONE, ERROR}
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
@@ -58,12 +60,13 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
             try {
                 val listResult = MarsApi.retrofitService.getProperties()
                 _properties.value = listResult
-                _status.value = "Success: ${listResult.size} elements retrieved"
+                _status.value = MarsApiStatus.DONE
             } catch (e: Exception ) {
-                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
             }
         }
     }
