9b2fbe75ae462b6d55bcab9c8b815a12ce86b176
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-06 15:29:54 +0000

    (Part 01) Add the Detail Screen

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -48,6 +48,19 @@ class OverviewViewModel : ViewModel() {
         get() = _properties
 
 
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+    val navigateToSelectedProperty : LiveData<MarsProperty>
+        get() = _navigateToSelectedProperty
+
+
+    fun displayPropertyDetails(property: MarsProperty) {
+        _navigateToSelectedProperty.value = property
+    }
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
+    }
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
